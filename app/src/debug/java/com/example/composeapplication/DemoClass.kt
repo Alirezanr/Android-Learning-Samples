e496779d756697f6f55f728e6502ee22b2a27a7d
Alireza Nori <alireza.nr1378@gmail.com>
2022-04-26 22:33:54 +0430

    Source sets.

---
+++ app/src/debug/java/com/example/composeapplication/DemoClass.kt
@@ -0,0 +1,6 @@
+package com.example.composeapplication
+
+//This class can be accessed only in debug mode.
+//How to create this source directory? R click in app->New->Directory
+class DemoClass {
+}
