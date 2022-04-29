2d9679b2fa0975d06248eb9a0b6b8988da91d400
Alireza Nori <alireza.nr1378@gmail.com>
2022-04-29 16:41:08 +0430

    Inject InputValidationsUseCases to MainViewModel.

--- app/src/main/java/com/example/composeapplication/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/composeapplication/presentation/MainViewModel.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import com.example.composeapplication.domain.use_case.InputValidationsUseCases
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val InputValidations: InputValidationsUseCases = InputValidationsUseCases()
+) : ViewModel() {
     var state by mutableStateOf(RegistrationFormState())
 }
