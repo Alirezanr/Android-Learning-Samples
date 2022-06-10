abdbab0a588299f2f2c9612cc21ba0dec835d523
Alireza Nori <alireza.nr1378@gmail.com>
2022-06-10 20:58:27 +0430

    Setup recycler view adapter. Setup viewBinding. Show a fake list.

---
+++ app/src/main/java/com/example/composeapplication/CharacterAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.composeapplication
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.composeapplication.databinding.RowCharacterViewholderBinding
+
+class CharacterAdapter(
+    val onCharacterClick: (CharacterModel) -> Unit
+) : RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
+
+    private val characterList = mutableListOf<CharacterModel>()
+
+    fun updateDataSet(characterList: List<CharacterModel>) {
+        this.characterList.clear()
+        this.characterList.addAll(characterList)
+        notifyDataSetChanged()
+    }
+
+    inner class CharacterViewHolder(private val itemBinding: RowCharacterViewholderBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(character: CharacterModel) {
+            val context = itemBinding.root.context
+            with(itemBinding) {
+                characterNameTextview.text = String.format(
+                    context.getString(R.string.name_placeholder),
+                    character.name
+                )
+                characterRoleTextview.text = String.format(
+                    context.getString(R.string.role_placeholder),
+                    character.role?.capitalize() ?: "Unknown"
+                )
+                orderOfThePhoenixTextview.text = String.format(
+                    context.getString(R.string.orderOfThePhoenix_placeholder),
+                    character.orderOfThePhoenix
+                )
+
+                characterInfoContainer.setOnClickListener {
+                    onCharacterClick(character)
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        val view = RowCharacterViewholderBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return CharacterViewHolder(view)
+    }
+
+
+    @SuppressLint("DefaultLocale")
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+        val character = characterList[position]
+        holder.bind(character)
+    }
+
+    override fun getItemCount() = characterList.size
+}
--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.composeapplication
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.composeapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val fakeData = List(15) { i ->
+            CharacterModel(
+                name = "name $i",
+                role = "some role",
+                orderOfThePhoenix = i % 2 == 0
+            )
+        }
+
+        binding.characterRecyclerview.apply {
+            adapter = CharacterAdapter {
+                Toast.makeText(this@MainActivity, it.name, Toast.LENGTH_SHORT).show()
+            }.also {
+                it.updateDataSet(fakeData)
+            }
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
 }
