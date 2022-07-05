bbcfc36c90c56c4d6c556ddf0de7528c0b2c1fbe
Alireza Nori <alireza.nr1378@gmail.com>
2022-07-05 12:36:22 +0430

    State handling and rendering a simple list.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,7 +3,23 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,10 +27,120 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeApplicationTheme {
-                Surface {
-
-                }
+                MyApp()
             }
         }
+    }
+}
+
+/*
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    heightDp = 720,
+)
+@Composable
+fun DefaultPreview() {
+    ComposeApplicationTheme {
+        MyApp()
+    }
+}*/
+
+
+@Composable
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    if (shouldShowOnboarding) {
+        OnBoardingScreen {
+            shouldShowOnboarding = false
+        }
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnBoardingScreen(
+    onContinueButtonClicked: () -> Unit
+) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Welcome to the Basics Codelab!",
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
+
+            Button(
+                onClick = onContinueButtonClicked,
+                modifier = Modifier.clip(RoundedCornerShape(5.dp))
+            ) {
+                Text(
+                    text = "Continue",
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greetings(names: List<String> = List(100) { "list num.$it" }) {
+    /* Column(
+         modifier = Modifier
+             .padding(vertical = 4.dp)
+     ) {
+         for (name in names) {
+             Greeting(name = name)
+         }
+     }*/
+
+    LazyColumn(
+        modifier = Modifier
+            .padding(vertical = 4.dp)
+    ) {
+        items(items = names) { name ->
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    val expanded = remember {
+        mutableStateOf(false)
+    }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(
+            vertical = 4.dp,
+            horizontal = 8.dp
+        )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+        }
+
     }
 }
