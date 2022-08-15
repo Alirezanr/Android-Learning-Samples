8aa76d1fe623544d73fd456457612a9b5527a0b1
Alireza Nori <alireza.nr1378@gmail.com>
2022-08-15 16:05:04 +0430

    Implement navigation drawer content.

---
+++ app/src/main/java/com/example/composeapplication/DrawerMenuItem.kt
@@ -0,0 +1,9 @@
+package com.example.composeapplication
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class DrawerMenuItem(
+    val id: String,
+    val title: String,
+    val icon: ImageVector
+)
