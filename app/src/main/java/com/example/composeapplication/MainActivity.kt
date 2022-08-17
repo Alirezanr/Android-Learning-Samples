bc4cb2b333ed5114a11d1136db123f1a863ca1b7
Alireza Nori <alireza.nr1378@gmail.com>
2022-08-17 23:28:51 +0430

    Use leakcanary and implement simple example.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.composeapplication
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        lateinit var context: Context
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        runBlocking {
+            delay(3000)
+            Intent(this@MainActivity, SecondActivity::class.java).also { intent ->
+                startActivity(intent)
+            }
+        }
     }
+
+
 }
