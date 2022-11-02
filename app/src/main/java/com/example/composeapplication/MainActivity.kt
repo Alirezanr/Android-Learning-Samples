0e75194705128495c6547247eb9aef45b7450ee8
Alireza Nori <alireza.nr1378@gmail.com>
2022-11-03 01:30:48 +0330

    Animate visibility.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.composeapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.animation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +26,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+
+                        val modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .padding(bottom = 14.dp)
+
+                        var isVisible by remember {
+                            mutableStateOf(false)
+                        }
+
+                        Button(onClick = {
+                            isVisible = !isVisible
+                        }) {
+                            Text(text = "Toggle")
+                        }
+
+                        AnimateVisibility(isVisible, modifier)
+
+                    }
+
                 }
             }
         }
@@ -30,14 +56,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeApplicationTheme {
-        Greeting("Android")
+fun AnimateVisibility(
+    isVisible: Boolean,
+    modifier: Modifier
+) {
+    //Wrapper composable to animate the visibility changes for our composable.
+    AnimatedVisibility(
+        visible = isVisible,
+        //The animation when composable becomes visible:
+        enter = slideInHorizontally() + fadeIn(),
+        //The animation when composable becomes invisible:
+        exit = slideOutHorizontally() + fadeOut(),
+        modifier = modifier
+    ) {
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+        )
     }
 }
