9a12ded4b0a036d8c5aca5285035700b338861bc
alireza_noori <alireza.nr1378@gmail.com>
2023-08-22 14:48:14 +0330

    Implement Ui.

---
+++ app/src/main/java/com/example/composeapplication/presentation/CountriesScreen.kt
@@ -0,0 +1,77 @@
+package com.example.composeapplication.presentation
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapplication.domain.model.SimpleCountry
+
+@Composable
+fun CountriesScreen(
+    state: CountriesViewModel.CountriesState,
+    onSelectCountry: (code: String) -> Unit,
+    onDismissDialog: () -> Unit,
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        if (state.isLoading) {
+            CircularProgressIndicator(
+                modifier = Modifier.align(Alignment.Center)
+            )
+        } else {
+            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                items(state.countries) { country ->
+                    CountryItem(country = country,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable { onSelectCountry(country.code) }
+                            .padding(16.dp)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CountryItem(
+    country: SimpleCountry,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = country.emoji,
+            fontSize = 30.sp
+        )
+
+        Spacer(modifier = Modifier.width(16.dp))
+
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
+            Text(
+                text = country.name,
+                fontSize = 24.sp
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                text = country.capital,
+                fontSize = 18.sp
+            )
+
+        }
+    }
+
+
+}
